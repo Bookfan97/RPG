55f645854335370fd92e2240b3b0b8f657bad54e
Noah Day <sheltielover432@gmail.com>
2019-01-19 22:13:54 -0500

    Collisions
    Map now is able to contain the temporary character sprite

---
+++ RPG/Assets/Scripts/Character.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Character : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+    protected Vector2 direction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    protected virtual void Update()
+    {
+        Move();
+    }
+    public void Move()
+    {
+        transform.Translate(direction * speed * Time.deltaTime);
+    }
+}
